16d3a0551434cd4a16360583cb16f27a47a5d2f9
james2323123 <james2323123@gmail.com>
2020-06-05 13:25:27 +0800

    hello

--- 03PARSER-PROEJCT-HANDOUT/compile.sh
+++
@@ -1,3 +0,0 @@
-yacc -d 'mini-pascal-yacc.y'
-lex 'mini-pascal-lex.l'
-cc 'y.tab.c' -ly -ll
--- 03PARSER-PROEJCT-HANDOUT/printOutput.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd output
-filenames=`ls -l | awk '{print $9}'`
-for filename in ${filenames} ; do
-	
-	echo -n  "${filename} : "
-	cat ${filename}
-
-done
