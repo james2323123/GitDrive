ceb31e0bf28e657139b793b52d968577a7ed8005
james2323123 <36201181+james2323123@users.noreply.github.com>
2023-07-18 15:40:44 +0800

    Add files via upload

---
+++ run_train.sh
@@ -0,0 +1,8 @@
+
+python run.py \
+--graph_size 100 \
+--baseline rollout \
+--run_name tsp100_rollout \
+--val_dataset data/tsp/tsp100_validation_seed4321.pkl \
+--hidden_dim 80 \
+--embedding_dim 80
